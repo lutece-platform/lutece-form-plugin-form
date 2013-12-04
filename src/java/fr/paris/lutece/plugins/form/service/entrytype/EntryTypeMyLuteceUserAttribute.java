d623ae5c84f8fcb83e7bf809b8b49f5f1c1127c2
vbroussard <vbroussard@users.noreply.github.com>
2013-12-04 09:26:02 +0000

    FORM-219 : Jalopy format.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/directory-form-workflow/plugin-form@54625 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/form/service/entrytype/EntryTypeMyLuteceUserAttribute.java
+++ src/java/fr/paris/lutece/plugins/form/service/entrytype/EntryTypeMyLuteceUserAttribute.java
@@ -46,13 +46,13 @@ import fr.paris.lutece.portal.service.security.UserNotSignedException;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.util.ReferenceList;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang.StringUtils;
 
 
 /**
@@ -64,16 +64,12 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
      * Name of the bean of this service
      */
     public static final String BEAN_NAME = "form.entryTypeMyLuteceUser";
-
     private static final String TEMPLATE_HTML_CODE = "admin/plugins/form/entries/html_code_entry_type_mylutece_user_attribute.html";
     private static final String TEMPLATE_CREATE = "admin/plugins/form/entries/create_entry_type_mylutece_user_attribute.html";
     private static final String TEMPLATE_MODIFY = "admin/plugins/form/entries/modify_entry_type_mylutece_user_attribute.html";
-
     private static final String PROPERTY_ENTRY_TITLE = "form.entryTypeMyLuteceUserAttribute.title";
-
     private static final String PARAMETER_DISPLAY_IN_FRONT_OFFICE = "display_front_office";
     private static final String PARAMETER_MYLUTECE_ATTRIBUTE_NAME = "mylutece_attribute_name";
-
     private ReferenceList _refListUserAttributes;
 
     /**
@@ -113,22 +109,23 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
 
         entry.setComment( StringUtils.EMPTY );
         entry.setConfirmField( Boolean.parseBoolean( request.getParameter( PARAMETER_DISPLAY_IN_FRONT_OFFICE ) ) );
-        entry.setMandatory( entry.isConfirmField( )
-                && Boolean.parseBoolean( request.getParameter( PARAMETER_MANDATORY ) ) );
+        entry.setMandatory( entry.isConfirmField(  ) &&
+            Boolean.parseBoolean( request.getParameter( PARAMETER_MANDATORY ) ) );
         entry.setCSSClass( request.getParameter( PARAMETER_CSS_CLASS ) );
         entry.setTitle( request.getParameter( PARAMETER_TITLE ) );
         entry.setHelpMessage( request.getParameter( PARAMETER_HELP_MESSAGE ) );
-        if ( entry.getFields( ) == null )
+
+        if ( entry.getFields(  ) == null )
         {
-            ArrayList<Field> listFields = new ArrayList<Field>( );
-            Field field = new Field( );
+            ArrayList<Field> listFields = new ArrayList<Field>(  );
+            Field field = new Field(  );
             listFields.add( field );
             entry.setFields( listFields );
         }
 
-        entry.getFields( ).get( 0 ).setValue( request.getParameter( PARAMETER_MYLUTECE_ATTRIBUTE_NAME ) );
-        entry.getFields( ).get( 0 ).setWidth( 50 );
-        entry.getFields( ).get( 0 ).setMaxSizeEnter( 0 );
+        entry.getFields(  ).get( 0 ).setValue( request.getParameter( PARAMETER_MYLUTECE_ATTRIBUTE_NAME ) );
+        entry.getFields(  ).get( 0 ).setWidth( 50 );
+        entry.getFields(  ).get( 0 ).setMaxSizeEnter( 0 );
 
         return null;
     }
@@ -138,35 +135,37 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
      */
     @Override
     public GenericAttributeError getResponseData( Entry entry, HttpServletRequest request, List<Response> listResponse,
-            Locale locale )
+        Locale locale )
     {
-        LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+        LuteceUser user = SecurityService.getInstance(  ).getRegisteredUser( request );
 
-        if ( user == null && SecurityService.isAuthenticationEnable( )
-                && SecurityService.getInstance( ).isExternalAuthentication( ) )
+        if ( ( user == null ) && SecurityService.isAuthenticationEnable(  ) &&
+                SecurityService.getInstance(  ).isExternalAuthentication(  ) )
         {
             try
             {
-                user = SecurityService.getInstance( ).getRemoteUser( request );
+                user = SecurityService.getInstance(  ).getRemoteUser( request );
             }
             catch ( UserNotSignedException e )
             {
-                AppLogService.error( e.getMessage( ), e );
+                AppLogService.error( e.getMessage(  ), e );
             }
         }
 
-        String strAttribute = entry.getFields( ).get( 0 ).getValue( );
-        if ( user == null
-                || ( StringUtils.isNotEmpty( strAttribute ) && StringUtils.isEmpty( user.getUserInfo( strAttribute ) ) ) )
+        String strAttribute = entry.getFields(  ).get( 0 ).getValue(  );
+
+        if ( ( user == null ) ||
+                ( StringUtils.isNotEmpty( strAttribute ) && StringUtils.isEmpty( user.getUserInfo( strAttribute ) ) ) )
         {
-            if ( entry.isMandatory( ) )
+            if ( entry.isMandatory(  ) )
             {
                 return new MandatoryError( entry, locale );
             }
+
             return null;
         }
 
-        Response response = new Response( );
+        Response response = new Response(  );
         response.setEntry( entry );
         response.setResponseValue( user.getUserInfo( strAttribute ) );
 
@@ -179,14 +178,14 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
      * Get a reference list with every lutece user attributes
      * @return The reference list with every user attributes
      */
-    public ReferenceList getLuteceUserAttributesRefList( )
+    public ReferenceList getLuteceUserAttributesRefList(  )
     {
         if ( _refListUserAttributes != null )
         {
             return _refListUserAttributes;
         }
 
-        ReferenceList referenceList = new fr.paris.lutece.util.ReferenceList( );
+        ReferenceList referenceList = new fr.paris.lutece.util.ReferenceList(  );
 
         referenceList.addItem( LuteceUser.BDATE, LuteceUser.BDATE );
         referenceList.addItem( LuteceUser.GENDER, LuteceUser.GENDER );
@@ -209,34 +208,31 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_STREET_NAME, LuteceUser.HOME_INFO_POSTAL_STREET_NAME );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_STREET_TYPE, LuteceUser.HOME_INFO_POSTAL_STREET_NAME );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_STREET_URBAN_DISTRICT,
-                LuteceUser.HOME_INFO_POSTAL_STREET_URBAN_DISTRICT );
+            LuteceUser.HOME_INFO_POSTAL_STREET_URBAN_DISTRICT );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_CITY, LuteceUser.HOME_INFO_POSTAL_CITY );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_STATEPROV, LuteceUser.HOME_INFO_POSTAL_STATEPROV );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_POSTALCODE, LuteceUser.HOME_INFO_POSTAL_POSTALCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_COUNTRY, LuteceUser.HOME_INFO_POSTAL_COUNTRY );
         referenceList.addItem( LuteceUser.HOME_INFO_POSTAL_ORGANIZATION, LuteceUser.HOME_INFO_POSTAL_ORGANIZATION );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_TELEPHONE_INTCODE,
-                LuteceUser.HOME_INFO_TELECOM_TELEPHONE_INTCODE );
+            LuteceUser.HOME_INFO_TELECOM_TELEPHONE_INTCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_TELEPHONE_LOCCODE,
-                LuteceUser.HOME_INFO_TELECOM_TELEPHONE_LOCCODE );
+            LuteceUser.HOME_INFO_TELECOM_TELEPHONE_LOCCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_TELEPHONE_NUMBER,
-                LuteceUser.HOME_INFO_TELECOM_TELEPHONE_NUMBER );
+            LuteceUser.HOME_INFO_TELECOM_TELEPHONE_NUMBER );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_TELEPHONE_EXT, LuteceUser.HOME_INFO_TELECOM_TELEPHONE_EXT );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_TELEPHONE_COMMENT,
-                LuteceUser.HOME_INFO_TELECOM_TELEPHONE_COMMENT );
+            LuteceUser.HOME_INFO_TELECOM_TELEPHONE_COMMENT );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_FAX_INT, LuteceUser.HOME_INFO_TELECOM_FAX_INT );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_FAX_LOCCODE, LuteceUser.HOME_INFO_TELECOM_FAX_LOCCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_FAX_NUMBER, LuteceUser.HOME_INFO_TELECOM_FAX_NUMBER );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_FAX_EXT, LuteceUser.HOME_INFO_TELECOM_FAX_EXT );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_FAX_COMMENT, LuteceUser.HOME_INFO_TELECOM_FAX_COMMENT );
-        referenceList
-                .addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_INTCODE, LuteceUser.HOME_INFO_TELECOM_MOBILE_INTCODE );
-        referenceList
-                .addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_LOCCODE, LuteceUser.HOME_INFO_TELECOM_MOBILE_LOCCODE );
+        referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_INTCODE, LuteceUser.HOME_INFO_TELECOM_MOBILE_INTCODE );
+        referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_LOCCODE, LuteceUser.HOME_INFO_TELECOM_MOBILE_LOCCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_NUMBER, LuteceUser.HOME_INFO_TELECOM_MOBILE_NUMBER );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_EXT, LuteceUser.HOME_INFO_TELECOM_MOBILE_EXT );
-        referenceList
-                .addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_COMMENT, LuteceUser.HOME_INFO_TELECOM_MOBILE_COMMENT );
+        referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_MOBILE_COMMENT, LuteceUser.HOME_INFO_TELECOM_MOBILE_COMMENT );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_PAGER_INTCODE, LuteceUser.HOME_INFO_TELECOM_PAGER_INTCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_PAGER_LOCCODE, LuteceUser.HOME_INFO_TELECOM_PAGER_LOCCODE );
         referenceList.addItem( LuteceUser.HOME_INFO_TELECOM_PAGER_NUMBER, LuteceUser.HOME_INFO_TELECOM_PAGER_NUMBER );
@@ -251,45 +247,43 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
         referenceList.addItem( LuteceUser.BUSINESS_INFO_POSTAL_POSTALCODE, LuteceUser.BUSINESS_INFO_POSTAL_POSTALCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_POSTAL_COUNTRY, LuteceUser.BUSINESS_INFO_POSTAL_COUNTRY );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_POSTAL_ORGANIZATION,
-                LuteceUser.BUSINESS_INFO_POSTAL_ORGANIZATION );
+            LuteceUser.BUSINESS_INFO_POSTAL_ORGANIZATION );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_INTCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_INTCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_INTCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_LOCCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_LOCCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_LOCCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_NUMBER,
-                LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_NUMBER );
+            LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_NUMBER );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_EXT,
-                LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_EXT );
+            LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_EXT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_COMMENT,
-                LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_COMMENT );
+            LuteceUser.BUSINESS_INFO_TELECOM_TELEPHONE_COMMENT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_INTCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_FAX_INTCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_FAX_INTCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_LOCCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_FAX_LOCCODE );
-        referenceList
-                .addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_NUMBER, LuteceUser.BUSINESS_INFO_TELECOM_FAX_NUMBER );
+            LuteceUser.BUSINESS_INFO_TELECOM_FAX_LOCCODE );
+        referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_NUMBER, LuteceUser.BUSINESS_INFO_TELECOM_FAX_NUMBER );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_EXT, LuteceUser.BUSINESS_INFO_TELECOM_FAX_EXT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_FAX_COMMENT,
-                LuteceUser.BUSINESS_INFO_TELECOM_FAX_COMMENT );
+            LuteceUser.BUSINESS_INFO_TELECOM_FAX_COMMENT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_INTCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_INTCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_INTCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_LOCCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_LOCCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_LOCCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_NUMBER,
-                LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_NUMBER );
-        referenceList
-                .addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_EXT, LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_EXT );
+            LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_NUMBER );
+        referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_EXT, LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_EXT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_COMMENT,
-                LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_COMMENT );
+            LuteceUser.BUSINESS_INFO_TELECOM_MOBILE_COMMENT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_PAGER_INTCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_PAGER_INTCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_PAGER_INTCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_PAGER_LOCCODE,
-                LuteceUser.BUSINESS_INFO_TELECOM_PAGER_LOCCODE );
+            LuteceUser.BUSINESS_INFO_TELECOM_PAGER_LOCCODE );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_PAGER_NUMBER,
-                LuteceUser.BUSINESS_INFO_TELECOM_PAGER_NUMBER );
+            LuteceUser.BUSINESS_INFO_TELECOM_PAGER_NUMBER );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_PAGER_EXT, LuteceUser.BUSINESS_INFO_TELECOM_PAGER_EXT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_TELECOM_PAGER_COMMENT,
-                LuteceUser.BUSINESS_INFO_TELECOM_PAGER_COMMENT );
+            LuteceUser.BUSINESS_INFO_TELECOM_PAGER_COMMENT );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_ONLINE_EMAIL, LuteceUser.BUSINESS_INFO_ONLINE_EMAIL );
         referenceList.addItem( LuteceUser.BUSINESS_INFO_ONLINE_URI, LuteceUser.BUSINESS_INFO_ONLINE_URI );
 
@@ -305,7 +299,7 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
     @Override
     public String getResponseValueForExport( Entry entry, HttpServletRequest request, Response response, Locale locale )
     {
-        return response.getResponseValue( );
+        return response.getResponseValue(  );
     }
 
     /**
@@ -314,6 +308,6 @@ public class EntryTypeMyLuteceUserAttribute extends AbstractEntryTypeMyLuteceUse
     @Override
     public String getResponseValueForRecap( Entry entry, HttpServletRequest request, Response response, Locale locale )
     {
-        return response.getResponseValue( );
+        return response.getResponseValue(  );
     }
 }
